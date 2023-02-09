dd8e2c1763b03fdc71b070624d6cd0792f8063f3
Kacper <kactop@st.amu.edu.pl>
2023-02-09 16:30:46 +0100

    +

---
+++ HttpClientSample/HttpClientSample/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HttpClientSample
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string? Login { get; set; }
+        [Display(Name = "Is Deleted?")]
+        public bool IsDeleted { get; set; }
+    }
+
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void ShowUser(User user)
+        {
+            Console.WriteLine($"Name: {user.Name}\tSurname: {user.Surname}\tDate Of Birth: {user.DateOfBirth}\tLogin: {user.Login}\tIs Deleted: {user.IsDeleted}");
+        }
+
+        static async Task<Uri> CreateUserAsync(User user)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/UsersApi", user);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<User> GetUserAsync(string path)
+        {
+            User? user = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                user = await response.Content.ReadAsAsync<User>();
+            }
+            return user;
+        }
+
+        static async Task<User> UpdateUserAsync(User user)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/UsersApi/{user.Id}", user);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated user from the response body.
+            user = await response.Content.ReadAsAsync<User>();
+            return user;
+        }
+
+        static async Task<User> DeleteUserAsync(User user)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/UsersApi/{user.Id}", user);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated user from the response body.
+            user = await response.Content.ReadAsAsync<User>();
+            return user;
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("https://localhost:7263/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {/*
+                // Create a new user
+                User user = new User
+                {
+                    Name = "Mario",
+                    Surname = "Bros",
+                    DateOfBirth = DateTime.Parse("2000-01-01"),
+                    Login = "mb@gmail.com",
+                    IsDeleted = false
+                };
+
+                var url = await CreateUserAsync(user);
+                Console.WriteLine($"Created at {url}");
+
+                // Get the user
+                user = await GetUserAsync(url.PathAndQuery);
+                ShowUser(user);
+
+                // Update the user
+                Console.WriteLine("Updating login...");
+                user.Login = "mario@gmail.com";
+                await UpdateUserAsync(user);
+
+                // Get the updated user
+                user = await GetUserAsync(url.PathAndQuery);
+                ShowUser(user);
+
+            */}
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/UsersApiController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly MvcMovieContext _context;
+
+        public UsersApiController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UsersApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+          if (_context.User == null)
+          {
+              return NotFound();
+          }
+            return await _context.User.ToListAsync();
+        }
+
+        // GET: api/UsersApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+          if (_context.User == null)
+          {
+              return NotFound();
+          }
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/UsersApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UsersApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+          if (_context.User == null)
+          {
+              return Problem("Entity set 'MvcMovieContext.User'  is null.");
+          }
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/UsersApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> DeleteUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
